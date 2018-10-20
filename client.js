99f335f73d12ebb4560c9a1d2c3cffb0e761eaad
luddite478 <luddite478@gmail.com>
2018-10-20 21:34:00 +0300

    first

--- client.js
+++ client.js
@@ -1,5 +1,4 @@
 const readline = require('readline');
-const request = require('request');
 const axios = require("axios");
 
 const rl = readline.createInterface({
@@ -14,70 +13,88 @@ if (process.stdin.isTTY){
 }
 
 process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'w') {
-        process.exit();
-  }
+  if (key.ctrl && key.name === 'q') process.exit();
 });
 
 rl.prompt();
 
-const phrases = [
-  'Press Enter if you want to perform transaction or press CTRL+W to exit',
-  'Enter network name (mainnet or devnet): ',
-  'Enter recipientId: ',
-  'Enter amount (in satoshis): ',
-  'Enter passphrase: '
-]
-
 console.log('Welcome to ark-client.');
 
-function* cliGen(){
-
-  const transactionParams = {
-      network: null,
-      recipientId: null,
-      amount: null,
-      passphrase: null
-  }
-
-  while(true){
-    yield ask(phrases[0]);
-
-    let network = yield ask(phrases[1]);
-    while(network !== "mainnet" && network !== "devnet"){
-        console.log("Only 2 networks available. Enter mainnet or devnet");
-        network = yield ask(phrases[1]);
+async function cliFlow() {
+  try {
+    const transactionParams = {
+        network: null,
+        recipientId: null,
+        amount: null,
+        passphrase: null
     }
-    transactionParams.recipientId = yield ask(phrases[2]);
-    transactionParams.amount = yield ask(phrases[3]);
-    while(transactionParams.amount < 1){
-        console.log("You can't send less then 1 satoshi");
-        transactionParams.amount = yield ask(phrases[3]);
-    }
-    transactionParams.passphrase = yield ask(phrases[4]);
 
-    const response = yield createTransaction(network, transactionParams);
-    console.log(response);
-    if(response.success){
-      console.log('great success!');
-    } else {
-      console.log(response.error);
+      await cliAsk('Press Enter if you want to perform transaction or press Ctrl+Q to exit\n');
+      let network = await cliAsk('Enter network name (mainnet or devnet): ');
+      while(network !== "mainnet" && network !== "devnet"){
+          console.log("Only 2 networks available");
+          network = await cliAsk('Enter network name (mainnet or devnet): ');
+      }
+      transactionParams.recipientId = await cliAsk('Enter recipientId: ');
+      transactionParams.amount = await cliAsk('Enter amount (in satoshis): ');
+      while(transactionParams.amount < 1){
+          console.log("You can't send less then 1 satoshi");
+          transactionParams.amount = await cliAsk('Enter amount (in satoshis): ');
+      }
+      transactionParams.passphrase = await cliAsk('Enter passphrase: ');
+
+      const creationResponse = await createTransaction(network, transactionParams);
+
+      if(creationResponse.data.success){
+        const transactionId = creationResponse.data.transaction.id;
+        await cliAsk(`Your transaction id is ${transactionId}\nPress Enter if you want to broadcast this transaction or Ctrl+Q to exit`);
+        await cliAsk('Select network again (mainnet or devnet): ');
+        while(network !== "mainnet" && network !== "devnet") {
+            console.log("Only 2 networks available");
+            network = await cliAsk('Enter network name (mainnet or devnet): ');
+        }
+
+        const broadcastingResponse = await broadcastTransaction(network, {id: transactionId});
+
+        if(broadcastingResponse.data.success){
+          console.log("Transaction successfully broadcasted\n");
+          cliFlow();
+        } else {
+          console.log("An error occurred during the broadcast: ", broadcastingResponse.data.error);
+          cliFlow();
+        }
+
+      } else {
+        console.log("An error occurred during the transaction creation: ", creationResponse.data.error);
+        cliFlow();
+      }
+
+    } catch(e){
+      console.log("Error! " + e);
+      process.exit();
     }
-  }
 }
 
-const createTransaction = async (network, transactionParams) => {
-  const response = await axios({
+cliFlow();
+
+function cliAsk(pharse) {
+  return new Promise((resolve) => {
+    rl.question(pharse, (line) => resolve(line))
+  })
+}
+
+async function createTransaction(network, transactionParams) {
+  return await axios({
     method:'post',
     url:`http://localhost:8080/${network}/transaction`,
     data: transactionParams
   })
-  console.log(response);
 }
 
-let cliFlow = cliGen();
-cliFlow.next();
-
-function ask(pharse){
-  rl.question(pharse, (line) => cliFlow.next(line))
+async function broadcastTransaction(network, transactionId) {
+  return await axios({
+    method:'post',
+    url:`http://localhost:8080/${network}/broadcast`,
+    data: transactionId
+  })
 }
